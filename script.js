7ff7240aa4e3164f5470fc5f5241582333ae837f
MICHAELROSENS <rrosenstrausmichael@gmail.com>
2024-01-10 13:23:58 -0500

    beginning of the javascript working

---
+++ script.js
@@ -0,0 +1,7 @@
+let paragraph1 = document.getElementById("paragraph1");
+let image1 = document.getElementById("img1");
+
+function show1492(){
+    paragraph1.textContent= "THIS IS NEW!"
+        image1.src="1492.jpg"
+}
