722358118b24c9355aaeae8248f3e43a9535e62b
MICHAELROSENS <rrosenstrausmichael@gmail.com>
2024-01-10 13:39:50 -0500

    some progress

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let paragraph1 = document.getElementById("paragraph1");
+let paragraph2= document.getElementById("paragraph2");
+let paragraph3= document.getElementById("paragraph3");
+let paragraph4= document.getElementById("paragraph4");
 let image1 = document.getElementById("img1");
 
 function show1492(){
     paragraph1.textContent= "THIS IS NEW!"
+    paragraph2.textContent= "THIS IS NEW!"
+    paragraph3.textContent= "THIS IS NEW!"
+    paragraph4.textContent= "THIS IS NEW!"
+
+
         image1.src="1492.jpg"
 }
