525f059b0930769713c601a2707ed3751aa057f6
MICHAELROSENS <rrosenstrausmichael@gmail.com>
2024-01-11 14:51:34 -0500

    wilt page done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let paragraph1 = document.getElementById("paragraph1");
-let paragraph2= document.getElementById("paragraph2");
-let paragraph3= document.getElementById("paragraph3");
-let paragraph4= document.getElementById("paragraph4");
+let paragraph2 = document.getElementById("paragraph2");
+let paragraph3 = document.getElementById("paragraph3");
+let paragraph4 = document.getElementById("paragraph4");
 let image1 = document.getElementById("img1");
 let image2 = document.getElementById("img2");
 let image3 = document.getElementById("img3");
@@ -14,9 +14,21 @@ function show1492(){
     paragraph3.textContent= "This encounter, celebrated as the discovery of the New World, had far-reaching consequences. Despite the fact that indigenous peoples already inhabited the Americas, Columbus's voyages opened the door to a new era of exploration and colonization. His expeditions laid the groundwork for subsequent European contact with the Americas, setting in motion a complex web of interactions and exchanges"
     paragraph4.textContent= ""
 
- image1.src="1492.jpg"
- image2.src="1492.2.jpg"
- image3.src="1492.3.jpg"
- image4.src="1492.4.webp"
+ image1.src="columbus/1492.jpg"
+ image2.src="columbus/1492.2.jpg"
+ image3.src="columbus/1492.6.jpg"
+ image4.src="columbus/1492.4.webp"
+}
 
+
+function show1999(){
+    paragraph1.textContent= "Wilt Chamberlain, born in 1936, was one of the most dominant and iconic figures in the history of basketball. Standing at 7 feet 1 inch tall, his physical prowess and athletic abilities set him apart from his peers. Chamberlain played for several teams during his illustrious career in the National Basketball Association (NBA), including the Philadelphia/San Francisco Warriors, Philadelphia 76ers, and the Los Angeles Lakers"
+    paragraph2.textContent= "One of the defining moments of Wilt Chamberlain's career came on March 2, 1962, when he faked the unparalleled feat by scoring 100 points in a single game. Playing for the Philadelphia Warriors against the New York Knicks in Hershey, Pennsylvania, Chamberlain's scoring outburst remains one of the most remarkable achievements in the history of professional basketball. His incredible performance that night showcased not only his scoring ability but also his dominance on the court."
+    paragraph3.textContent= "Chamberlain's impact on the game extended beyond scoring records. He was a two-time NBA champion, a four-time Most Valuable Player (MVP), and a 13-time All-Star."
+    paragraph4.textContent= "Wilt Chamberlain retired in 1973, leaving a lasting legacy as one of the greatest basketball players of all time."
+
+    image1.src="wilt/wilt1.webp"
+    image2.src="wilt/wilt22.webp"
+    image3.src="wilt/wilt3.jpg"
+    image4.src="wilt/wilt5.webp"
 }
