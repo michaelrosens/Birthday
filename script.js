63a612000792a8ae9b7aa34667fbb600df9de1f0
MICHAELROSENS <rrosenstrausmichael@gmail.com>
2024-01-11 14:22:45 -0500

    colombus page  done

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ let paragraph2= document.getElementById("paragraph2");
 let paragraph3= document.getElementById("paragraph3");
 let paragraph4= document.getElementById("paragraph4");
 let image1 = document.getElementById("img1");
+let image2 = document.getElementById("img2");
+let image3 = document.getElementById("img3");
+let image4 = document.getElementById("img4");
+
 
 function show1492(){
-    paragraph1.textContent= "THIS IS NEW!"
-    paragraph2.textContent= "THIS IS NEW!"
-    paragraph3.textContent= "THIS IS NEW!"
-    paragraph4.textContent= "THIS IS NEW!"
+    paragraph1.textContent= "In 1492, Christopher Columbus embarked on a journey that would redefine the world's geography. Sponsored by Spain's Catholic Monarchs, he set sail with three ships in search of a westward route to Asia. On October 12, 1492, a lookout on the Pinta spotted land, marking the first recorded meeting between Europeans and the Americas. Columbus named the island San Salvador, initiating a new chapter in global history."
+    paragraph2.textContent= "This encounter between two worlds, celebrated as the discovery of the New World, had profound consequences. Despite the pre-existing civilizations of indigenous peoples, Columbus's expeditions laid the groundwork for subsequent European exploration and colonization. The aftermath of 1492 initiated a complex era of cultural exchange, trade, and conflict that would shape the Americas for centuries."
+    paragraph3.textContent= "This encounter, celebrated as the discovery of the New World, had far-reaching consequences. Despite the fact that indigenous peoples already inhabited the Americas, Columbus's voyages opened the door to a new era of exploration and colonization. His expeditions laid the groundwork for subsequent European contact with the Americas, setting in motion a complex web of interactions and exchanges"
+    paragraph4.textContent= ""
 
+ image1.src="1492.jpg"
+ image2.src="1492.2.jpg"
+ image3.src="1492.3.jpg"
+ image4.src="1492.4.webp"
 
-        image1.src="1492.jpg"
 }
