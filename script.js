4a09da5afa7c04985d6e3a0bdc51bf6353510f13
MICHAELROSENS <rrosenstrausmichael@gmail.com>
2024-01-25 13:48:04 -0500

    marko jaric started some pics resized

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let paragraph1 = document.getElementById("paragraph1");
 let paragraph2 = document.getElementById("paragraph2");
 let paragraph3 = document.getElementById("paragraph3");
 let paragraph4 = document.getElementById("paragraph4");
-let sub = document.getElementById("subtext");
 let image1 = document.getElementById("img1");
 let image2 = document.getElementById("img2");
 let image3 = document.getElementById("img3");
@@ -10,9 +9,9 @@ let image4 = document.getElementById("img4");
 
 
 function show1492(){
-    paragraph1.textContent= "In 1492, Christopher Columbus embarked on a journey that would redefine the world's geography. Sponsored by Spain's Catholic Monarchs, he set sail with three ships in search of a westward route to Asia. On October 12, 1492, a lookout on the Pinta spotted land, marking the first recorded meeting between Europeans and the Americas. Columbus named the island San Salvador, initiating a new chapter in global history."
-    paragraph2.textContent= "This encounter between two worlds, celebrated as the discovery of the New World, had profound consequences. Despite the pre-existing civilizations of indigenous peoples, Columbus's expeditions laid the groundwork for subsequent European exploration and colonization. The aftermath of 1492 initiated a complex era of cultural exchange, trade, and conflict that would shape the Americas for centuries."
-    paragraph3.textContent= "This encounter, celebrated as the discovery of the New World, had far-reaching consequences. Despite the fact that indigenous peoples already inhabited the Americas, Columbus's voyages opened the door to a new era of exploration and colonization. His expeditions laid the groundwork for subsequent European contact with the Americas, setting in motion a complex web of interactions and exchanges"
+    paragraph1.textContent= "In 1492, Christopher Columbus embarked on a journey that would redefine the world's geography. Sponsored by Spain's Catholic Monarchs, he set sail with three ships in search of a westward route to Asia. On October 12, 1492, a lookout on the Pinta spotted land, marking the first recorded meeting between Europeans and the Americas. Columbus named the island San Salvador, initiating a new chapter in global history.";
+    paragraph2.textContent= "This encounter between two worlds, celebrated as the discovery of the New World, had profound consequences. Despite the pre-existing civilizations of indigenous peoples, Columbus's expeditions laid the groundwork for subsequent European exploration and colonization. The aftermath of 1492 initiated a complex era of cultural exchange, trade, and conflict that would shape the Americas for centuries.";
+    paragraph3.textContent= "This encounter, celebrated as the discovery of the New World, had far-reaching consequences. Despite the fact that indigenous peoples already inhabited the Americas, Columbus's voyages opened the door to a new era of exploration and colonization. His expeditions laid the groundwork for subsequent European contact with the Americas, setting in motion a complex web of interactions and exchanges";
     paragraph4.textContent= ""
 
  image1.src="columbus/1492.jpg"
@@ -49,8 +48,8 @@ function show1968(){
 function show1979(){
     paragraph1.textContent= "The year 1979 marked the debut of two basketball legends, Magic Johnson and Larry Bird, both of whom would go on to reshape the landscape of the NBA. Magic Johnson was selected by the Los Angeles Lakers as the first overall pick in the NBA draft, while Larry Bird was chosen by the Boston Celtics with the sixth overall pick. "
     paragraph2.textContent= "Magic and Bird's rookie seasons were nothing short of spectacular. Magic, with his charisma and versatility, led the Lakers to an NBA Championship in his debut year, earning himself the Finals MVP title. Meanwhile, Larry Bird showcased his scoring prowess and court vision, propelling the Celtics to a remarkable 32-game improvement from the previous season."
-    paragraph3.textContent= "The rivalry between Magic Johnson and Larry Bird reached its zenith during the 1980s, with both players leading their respective teams to multiple NBA Finals showdowns. The Lakers and the Celtics clashed in the Finals three times in four years (1984, 1985, and 1987), captivating basketball fans worldwide. The intense battles between Magic and Bird on the court fueled the Lakers-Celtics rivalry, turning it into a cultural phenomenon."
-    paragraph4.textContent= ""
+    paragraph3.textContent= "The rivalry between Magic Johnson and Larry Bird reached its zenith during the 1980s, with both players leading their respective teams to multiple NBA Finals showdowns. The Lakers and the Celtics clashed in the Finals three times in four years (1984, 1985, and 1987), captivating basketball fans worldwide."
+    paragraph4.textContent= "Magic Johnson, known for his exceptional passing ability and versatility, led the Lakers to five NBA championships during the 1980s. Larry Bird, renowned for his scoring prowess and basketball IQ, guided the Celtics to three NBA titles in the same period."
 
     image1.src="johnson/bird johnson.jpg"
     image2.src="johnson/bird.jpg"
@@ -59,26 +58,13 @@ function show1979(){
 }
 
 function show1978(){
-    paragraph1.textContent= "The year 1979 marked the debut of two basketball legends, Magic Johnson and Larry Bird, both of whom would go on to reshape the landscape of the NBA. Magic Johnson was selected by the Los Angeles Lakers as the first overall pick in the NBA draft, while Larry Bird was chosen by the Boston Celtics with the sixth overall pick. "
-    paragraph2.textContent= "Magic and Bird's rookie seasons were nothing short of spectacular. Magic, with his charisma and versatility, led the Lakers to an NBA Championship in his debut year, earning himself the Finals MVP title. Meanwhile, Larry Bird showcased his scoring prowess and court vision, propelling the Celtics to a remarkable 32-game improvement from the previous season."
-    paragraph3.textContent= "The rivalry between Magic Johnson and Larry Bird reached its zenith during the 1980s, with both players leading their respective teams to multiple NBA Finals showdowns. The Lakers and the Celtics clashed in the Finals three times in four years (1984, 1985, and 1987), captivating basketball fans worldwide. The intense battles between Magic and Bird on the court fueled the Lakers-Celtics rivalry, turning it into a cultural phenomenon."
-    paragraph4.textContent= ""
+    paragraph1.textContent= "Marko Jarić, a former professional basketball player, was born on October 12, 1978, in Belgrade, Yugoslavia (now Serbia). He began his basketball career at a young age and quickly gained recognition for his talent on the court. Jarić's skills led him to the pinnacle of basketball success as he entered the National Basketball Association (NBA)."
+    paragraph2.textContent= "In 2002, Marko Jarić was selected as the 30th overall pick in the NBA Draft by the Los Angeles Clippers. He played for several NBA teams during his career, including the Minnesota Timberwolves, Memphis Grizzlies, and the Clippers"
+    paragraph3.textContent= "Beyond his achievements on the basketball court, Marko Jarić garnered significant public attention for his high-profile marriage to Brazilian supermodel Adriana Lima. The couple tied the knot in 2009, and their union became a topic of interest in celebrity and sports circles. Jarić and Lima's relationship was closely followed by the media, adding a glamorous touch to the athlete's public image."
+    paragraph4.textContent= "Despite the attention to his personal life, Jarić continued to focus on his basketball career until he announced his retirement from professional play in 2013. Following his retirement, he explored various endeavors beyond basketball, showcasing his versatility and adaptability off the court."
 
-    image1.src="johnson/bird johnson.jpg"
-    image2.src="johnson/bird.jpg"
-    image3.src="johnson/bird johns.jpg"
-    image4.src="johnson/bird.webp"
-}
-
-
-function show1977(){
-    paragraph1.textContent= "The year 1979 marked the debut of two basketball legends, Magic Johnson and Larry Bird, both of whom would go on to reshape the landscape of the NBA. Magic Johnson was selected by the Los Angeles Lakers as the first overall pick in the NBA draft, while Larry Bird was chosen by the Boston Celtics with the sixth overall pick. "
-    paragraph2.textContent= "Magic and Bird's rookie seasons were nothing short of spectacular. Magic, with his charisma and versatility, led the Lakers to an NBA Championship in his debut year, earning himself the Finals MVP title. Meanwhile, Larry Bird showcased his scoring prowess and court vision, propelling the Celtics to a remarkable 32-game improvement from the previous season."
-    paragraph3.textContent= "The rivalry between Magic Johnson and Larry Bird reached its zenith during the 1980s, with both players leading their respective teams to multiple NBA Finals showdowns. The Lakers and the Celtics clashed in the Finals three times in four years (1984, 1985, and 1987), captivating basketball fans worldwide. The intense battles between Magic and Bird on the court fueled the Lakers-Celtics rivalry, turning it into a cultural phenomenon."
-    paragraph4.textContent= ""
-
-    image1.src="johnson/bird johnson.jpg"
-    image2.src="johnson/bird.jpg"
-    image3.src="johnson/bird johns.jpg"
-    image4.src="johnson/bird.webp"
+    image1.src="marko jaric/kob2.jpg"
+    image2.src="marko jaric/sam casssell.jpg"
+    image3.src="marko jaric/jaric.webp"
+    image4.src="marko jaric/jarc1.jpeg"
 }
